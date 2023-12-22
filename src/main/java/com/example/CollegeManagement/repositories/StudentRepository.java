917821b6b0a365f9c391bdd530c4183cac3114b5
hanniel09 <hannielvieira1227@gmail.com>
2023-12-22 11:21:19 -0300

    feat: update repository's

--- src/main/java/com/example/CollegeManagement/repositories/StudentRepository.java
+++ src/main/java/com/example/CollegeManagement/repositories/StudentRepository.java
@@ -1,7 +1,12 @@
 package com.example.CollegeManagement.repositories;
 
+import com.example.CollegeManagement.models.Course;
 import com.example.CollegeManagement.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Course> findCourses(List<Course> courseList);
 }
