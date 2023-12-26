2122de539580621b2f7ff95c2bfc778ecb535c46
hanniel09 <hannielvieira1227@gmail.com>
2023-12-26 10:39:03 -0300

    feat: update Student service

--- src/main/java/com/example/CollegeManagement/services/StudentService.java
+++ src/main/java/com/example/CollegeManagement/services/StudentService.java
@@ -6,6 +6,7 @@ import com.example.CollegeManagement.models.Student;
 import com.example.CollegeManagement.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,9 +23,21 @@ public class StudentService {
         return studentRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
+    @Transactional
     public Student createStudent(StudentDTO data){
         Student student = new Student(data);
         return studentRepository.save(student);
+    }
+
+    public Student updateStudent(StudentDTO data, Long id){
+        Student newStudent = new Student(data);
+        Student updateStudent = findStudentById(id);
+        newStudent.setStudentId(updateStudent.getStudentId());
+        return studentRepository.save(newStudent);
+    }
+
+    public void deleteStudent(Long id){
+        studentRepository.deleteById(id);
     }
 
 }
